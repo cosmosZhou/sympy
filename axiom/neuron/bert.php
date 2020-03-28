404adfb14e55e60fa5f2d5cbb717f82ec987fe60
unknown <744984949@qq.com>
2020-03-29 00:14:25 +0800

    delete 2 .php files

--- axiom/neuron/bert.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '..\..\utility.php';
-$i = 0;
-$txt[$i++] = "\\(S = \\operatorname{softmax}{\\left(\\frac{Q \\times K^{\\color{red} T}}{\\sqrt{d}} \\right)} \\times V\\tag*{Eq[0]}\\)\\({S}_{0} = \\operatorname{softmax}{\\left(\\frac{{Q}_{0} \\times K^{\\color{red} T}}{\\sqrt{d}} \\right)} \\times V\\tag*{Eq[1]}\\)";
-$txt[$i++] = "\\({S}_{0} = \\operatorname{softmax}{\\left(\\frac{{Q}_{0} \\times K^{\\color{red} T}}{\\sqrt{d}} \\right)} \\times V\\tag*{Eq[1]}\\)";
-render(__FILE__, $txt);
-?>        
-
