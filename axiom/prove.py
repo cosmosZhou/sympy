98eb76dfbc750d5da27cfa97e3f518e8dd06a078
asus <744984949@qq.com>
2020-05-24 20:40:24 +0800

    prove.py updated

--- axiom/prove.py
+++ axiom/prove.py
@@ -28,7 +28,7 @@ def readFolder(rootdir, sufix='.py'):
             if name == '__init__':
                 continue
 
-            if 'index_j' not in name:
+            if 'factorial' not in name:
                 continue 
             print('testing', path)
             path = re.compile(r'[\\/]+').split(path[:-len(sufix)])
