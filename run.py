1f07e08e006013ef619678f1a32623a3f8da7e15
Lizhi Zhou <zhoulizhi@patsnap.com>
2020-08-14 18:40:55 +0800

    export WOLFRAM_INSTALLATION_DIRECTORY=D:\Program Files\Wolfram Research\Mathematica\12.1

--- run.py
+++ run.py
@@ -1,10 +1,20 @@
 # coding=utf-8
 import axiom
 import sys
+from wolframclient.language import wlexpr
+from wolframclient.evaluation.kernel.localsession import WolframLanguageSession
 # to run this script, please install:
-# pip install mpmath==1.1.0       
-
+# pip install mpmath==1.1.0
 if __name__ == '__main__':
+#     cd D:/Program Files/Wolfram Research/Mathematica/12.1/SystemFiles/Components/WolframClientForPython
+#     pip install .
+
+# export WOLFRAM_INSTALLATION_DIRECTORY=D:\Program Files\Wolfram Research\Mathematica\12.1
+
+    session = WolframLanguageSession()
+
+    limit = wlexpr('Limit[Sin[x] / x, x -> 0]')
+    print('session.evaluate(limit) =', session.evaluate(limit))
     if len(sys.argv) == 1:
         axiom.prove.prove()
     else:
