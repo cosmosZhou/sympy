486c1089365786231f918ad6b22ef87010d24b1b
Cosmos <zhoulizhi@patsnap.com>
2020-10-25 17:07:40 +0800

    <br>

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 include_once 'index.html';
 
 ?>
-welcome to http://www.cosmosstudio.cn/sympy/
-www.cosmosstudio.cn is a website for mathematical axioms proving system,
+welcome to http://www.cosmosstudio.cn/sympy/<br>
 
-based on open-source symbolic computation project of sympy and Maxima, as well as on commercial algebraic system mathematica.$_COOKIE
+www.cosmosstudio.cn is a website for mathematical axioms proving system, <br>
 
-on which the low-level computation is based. 
+based on open-source symbolic computation project of sympy and Maxima, as well as on commercial algebraic system namely, Mathematica <br>
+
+on which the low-level computation of cosmosstudio.cn is based. <br>
 
