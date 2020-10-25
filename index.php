ac70ac4eeef1cbeaa2fc40a95fcfaf5aebbd2d11
Cosmos <zhoulizhi@patsnap.com>
2020-10-25 17:03:40 +0800

    welcome

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 include_once 'index.html';
+
+welcome to http://www.cosmosstudio.cn/sympy/
+www.cosmosstudio.cn is a website for mathematical axioms proving system,
+
+based on open-source symbolic computation project of sympy and Maxima, as well as on commercial algebraic system mathematica.$_COOKIE
+
+on which the low-level computation is based. 
 ?>
