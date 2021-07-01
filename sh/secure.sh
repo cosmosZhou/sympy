04244e25d7cc7de235396b6f0633b8ab7f0c5d8c
Cosmos.Zhou <744984949@qq.com>
2021-07-01 17:13:53 +0800

    run with cgi in apache

---
+++ sh/secure.sh
@@ -0,0 +1,13 @@
+sed -i 's/#LoadModule ssl_module modules\/mod_ssl.so/LoadModule ssl_module modules\/mod_ssl.so/' /usr/local/httpd/conf/httpd.conf
+
+sleep 1
+
+sed -i 's/#Include conf\/extra\/httpd-ssl.conf/Include conf\/extra\/httpd-ssl.conf/' /usr/local/httpd/conf/httpd.conf
+
+sleep 1
+
+sed -i -E 's/#RewriteRule (.+)/RewriteRule \1/' /usr/local/httpd/conf/httpd.conf
+
+sleep 1
+
+/usr/local/httpd/bin/apachectl -k restart
