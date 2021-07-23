7bfbd09797f117c28e48ee92f07c9f70479ad2d9
Cosmos.Zhou <744984949@qq.com>
2021-07-23 09:10:30 +0800

    delete wolfram-client

--- wolframclient/deserializers/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import, print_function, unicode_literals
-
-from wolframclient.deserializers.wxf import binary_deserialize
-from wolframclient.deserializers.wxf.wxfconsumer import WXFConsumer, WXFConsumerNumpy
-from wolframclient.deserializers.wxf.wxfparser import WXFToken
-
-__all__ = ["WXFConsumer", "WXFToken", "binary_deserialize", "WXFConsumer", "WXFConsumerNumpy"]
--- wolframclient/settings.py
+++
@@ -1,3 +0,0 @@
-from __future__ import absolute_import, print_function, unicode_literals
-
-NOT_PROVIDED = object()
